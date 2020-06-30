ff7f5c6b4913bcd41259e45c1036ade54bb77563
thisistrivial <g.lee13770334@gmail.com>
2020-06-29 22:42:50 -0400

    Added MainActivity.java structure

--- app/src/main/java/com/example/githubcontributionswallpaper/MainActivity.java
+++ app/src/main/java/com/example/githubcontributionswallpaper/MainActivity.java
@@ -3,12 +3,62 @@ package com.example.githubcontributionswallpaper;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText github_username;
+    TextView github_status;
+    ImageView github_graph;
+    Switch github_wallpaper_switch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        github_username = (EditText) findViewById(R.id.github_username);
+        github_status = (TextView) findViewById(R.id.github_status);
+        github_graph = (ImageView) findViewById(R.id.github_graph);
+        github_wallpaper_switch = (Switch) findViewById(R.id.github_wallpaper_switch);
+
+        github_username.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void afterTextChanged(Editable s) {}
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start,
+                                          int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start,
+                                      int before, int count) {
+                if(s.length() == 0) {
+                    return;
+                }
+                String input = s.toString();
+                // TODO JSoup
+                // TODO set status
+                // TODO set graph
+            }
+        });
+
+        github_wallpaper_switch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    // TODO set wallpaper
+                } else {
+                    // TODO remove wallpaper
+                }
+            }
+        });
     }
 }
